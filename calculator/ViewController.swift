5e2e8f4cd165653f5df8cc2e70b5193a8b9f2339
Abilat01 <Abilat01@yandex.ru>
2021-08-07 20:42:42 +0300

    Logic

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var numberDoble: Double = 0
+    
+    @IBOutlet weak var resulLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func digits(_ sender: UIButton) {
+        
+        resulLabel.text = resulLabel.text! + String(sender.tag)
+        numberDoble = Double(resulLabel.text!)!
+    }
+    @IBAction func buttonsMathematicalOperations(_ sender: Any) {
+        
+    }
+    
 }
 
